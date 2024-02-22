096eac59109617cd7ca4671285db76d7a644b13f
Idan Beck <idanbeck@Idans-MacBook-Pro.local>
2024-02-22 14:05:18 -0800

    xcode build mostly working, a bit messy, but it's ok

--- Editor/PostBuildEpoch.cs
+++ Editor/PostBuildEpoch.cs
@@ -1,7 +1,9 @@
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
+using UnityEditor.PackageManager;
 
 public class XcodeProjectModifier {
     [PostProcessBuild]
@@ -12,23 +14,65 @@ public class XcodeProjectModifier {
 
             PBXProject proj = new PBXProject();
             proj.ReadFromString(File.ReadAllText(projPath));
-
+			
 #if UNITY_2019_3_OR_NEWER
-            string targetGuid = proj.GetUnityFrameworkTargetGuid();
+			string unityTargetGuid = proj.GetUnityMainTargetGuid();
+			string unityFrameworkTargetGuid = proj.GetUnityFrameworkTargetGuid();
 #else
-            string targetGuid = proj.TargetGuidByName("Unity-iPhone");
+			string unityTargetGuid = proj.TargetGuidByName("Unity-iPhone");
+			string unityFrameworkTargetGuid = proj.TargetGuidByName("Unity-iPhone");
 #endif
 
+
 			string epochLibGuid = proj.AddFile("libepoch_cli_lib.a", "Libraries/libepoch_cli_lib.a", PBXSourceTree.Sdk);
-			proj.AddFileToBuild(targetGuid, epochLibGuid); 
+			proj.AddFileToBuild(unityFrameworkTargetGuid, epochLibGuid); 
+
+			// Frameworks path by way of EpochButton class 
+			var packageInfo = UnityEditor.PackageManager.PackageInfo.FindForAssembly(typeof(XcodeProjectModifier).Assembly);
+			string packageRuntimePath = packageInfo.assetPath;
+            string frameworksRelativePath = "./Plugins/Apple/ios"; 
+            string frameworksPath = Path.Combine(packageRuntimePath, frameworksRelativePath);
+
+			// Frameworks 
+			var frameworksDir = Path.Combine(pathToBuiltProject, "Frameworks");
+			if (!Directory.Exists(frameworksDir))
+    			Directory.CreateDirectory(frameworksDir);
+
+			var librariesDir = Path.Combine(pathToBuiltProject, "Libraries");
+			if (!Directory.Exists(librariesDir))
+    			Directory.CreateDirectory(librariesDir);
+
+			var xcframeworks = new string[] {
+    			"libavcodec.xcframework",
+    			"libavdevice.xcframework",
+    			"libavfilter.xcframework",
+    			"libavformat.xcframework",
+    			"libavutil.xcframework",
+    			"libswresample.xcframework",
+    			"libswscale.xcframework"
+			};
 			
-			proj.AddFrameworkToProject(targetGuid, "libavcodec.xcframework", false);
-			proj.AddFrameworkToProject(targetGuid, "libavdevice.xcframework", false);
-			proj.AddFrameworkToProject(targetGuid, "libavfilter.xcframework", false);
-			proj.AddFrameworkToProject(targetGuid, "libavformat.xcframework", false);
-			proj.AddFrameworkToProject(targetGuid, "libavutil.xcframework", false);
-			proj.AddFrameworkToProject(targetGuid, "libswresample.xcframework", false);
-			proj.AddFrameworkToProject(targetGuid, "libswscale.xcframework", false);
+		//string fileGuid = project.AddFile(destPath, destPath);
+        //project.AddFileToEmbedFrameworks(targetGuid, fileGuid);
+
+			//string frameworksBuildPhaseGUID = proj.GetFrameworksBuildPhaseByTarget(unityTargetGuid);
+
+			foreach (var xcframework in xcframeworks) {
+    			var srcPath = Path.Combine(frameworksPath, xcframework); // Update with the actual path to your .xcframeworks
+    			//var destPath = Path.Combine(frameworksDir, xcframework);
+
+				var destPath = Path.Combine(librariesDir, xcframework);
+    			FileUtil.CopyFileOrDirectory(srcPath, destPath);
+
+				//var libFrameworkPath = 
+				//proj.AddFileToBuild(targetGuid, epochLibGuid);
+				
+				string xcframeworkGuid = proj.AddFile(destPath, destPath, PBXSourceTree.Sdk);
+				UnityEditor.iOS.Xcode.Extensions.PBXProjectExtensions.AddFileToEmbedFrameworks(proj, unityTargetGuid, xcframeworkGuid); 
+				
+				// add to project
+				//proj.AddFrameworkToProject(targetGuid, xcframework, false);
+			}
 
             File.WriteAllText(projPath, proj.WriteToString());
         }
